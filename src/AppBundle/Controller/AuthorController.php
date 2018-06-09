ecc2e6edee8a0b33b09b090fb712d2d3a63a6ec2
Ilyamander los Ilyatos <mrend@icloud.com>
2018-06-10 00:00:13 +0300

    new way to use EntityManager

--- src/AppBundle/Controller/AuthorController.php
+++ src/AppBundle/Controller/AuthorController.php
@@ -10,39 +10,35 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Author;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthorController extends Controller
 {
-    public function addAction($name)
+    public function addAction(EntityManagerInterface $em, $name)
     {
-        // you can fetch the EntityManager via $this->getDoctrine()
-        // or you can add an argument to your action: createAction(EntityManagerInterface $entityManager)
-        $entityManager = $this->getDoctrine()->getManager();
-
         $author = new Author();
         $author->setName($name);
 
         // tells Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($author);
+        $em->persist($author);
 
         // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
+        $em->flush();
 
         return $this->render('author/add_message.html.twig', [
             'id' => $author->getId(),
         ]);
-        //return new Response('Saved new author with id '.$author->getId());
     }
 
     public function deleteAction()
     {
-
+        return new Response('null');
     }
 
     public function listAction()
     {
-
+        return new Response('null');
     }
 }
